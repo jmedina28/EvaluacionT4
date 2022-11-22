46604a7c4401bbbcac800312577ab2987ba4273e
jmedina28 <juanperezmedina67@gmail.com>
2022-11-22 20:49:07 +0100

    creo el constructor de la clase cola y la funcion que devuelve una cola vacia

--- cola.py
+++ cola.py
@@ -1,4 +1,14 @@
-class nodoCola:
-    def __init__(self, dato):
-        self.dato = dato
+class nodoCola():
+    def __init__(self):
+        self.dato = None
         self.siguiente = None
+
+class Cola():
+
+    def __init__(self):
+        self.frente = None 
+        self.ultimo = None
+        self.nelementos = 0
+
+def vacia(cola):
+    return cola.nelementos == None
