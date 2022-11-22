f47453ca547e53517a8abda2f1b1a31b57565c31
jmedina28 <juanperezmedina67@gmail.com>
2022-11-22 21:01:19 +0100

    explico nrr

--- arbolbinario.py
+++ arbolbinario.py
@@ -1,7 +1,7 @@
 from cola import Cola, vacia, atencion, arribo
 
 class nodoArbol(object):
-
+    # nrr significa numero de registro relativo
     def __init__(self, info, nrr=None):
         self.izq, self.der = None, None
         self.info, self.nrr = info, nrr
