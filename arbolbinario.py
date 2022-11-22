1814347be67efb83ec89e436a71ac7eb1017373c
jmedina28 <juanperezmedina67@gmail.com>
2022-11-22 21:06:47 +0100

    dejo creada la funcion insertar para añadir nodos al arbol que vamos a construir

--- arbolbinario.py
+++ arbolbinario.py
@@ -11,3 +11,13 @@ class nodoHuffman(object):
     def __init__(self, info, valor):
         self.izq, self.der = None, None
         self.info, self.valor = info, valor
+
+def insertar(raiz, dato, nrr=None):
+    if(raiz is None):
+        raiz = nodoArbol(dato, nrr)
+    else:
+        if(raiz.info <= dato):
+            raiz.der = insertar(raiz.der, dato, nrr)
+        else:
+            raiz.izq = insertar(raiz.izq, dato, nrr)
+    return raiz
