4fba5faabb397c2b1ec3838d115979c639871b9d
jmedina28 <juanperezmedina67@gmail.com>
2022-11-23 22:21:49 +0100

    doy formato al código

--- ejer1.py
+++ ejer1.py
@@ -1,18 +1,23 @@
 from arbolbinario import insertar, barridopostorden, nodoHuffman
 from sys import getsizeof
 
-dic = {'A': '00', '3': '01', '1': '100', 'T': '110', 
-    'F': '111','0': '1010', 'M': '1011'}
+dic = {'A': '00', '3': '01', '1': '100', 'T': '110',
+       'F': '111', '0': '1010', 'M': '1011'}
 
-tabla = [['A', 0.2], ['F', 0.17], ['1', 0.13], ['3', 0.21], ['0', 0.05], ['M', 0.09], ['T', 0.15]]
+tabla = [['A', 0.2], ['F', 0.17], ['1', 0.13], [
+    '3', 0.21], ['0', 0.05], ['M', 0.09], ['T', 0.15]]
+
 
 def comparo(elemento):
     return elemento[1]
 
+
 tabla.sort(key=comparo)
+
 
 def comparacionnodo(elemento):
     return elemento.valor
+
 
 bosque = []
 
@@ -31,6 +36,7 @@ while(len(bosque) > 1):
     bosque.append(nodo)
     bosque.sort(key=comparacionnodo)
 
+
 def gtabla(raiz, cadena=''):
     if(raiz is not None):
         if(raiz.izq is None):
@@ -42,7 +48,9 @@ def gtabla(raiz, cadena=''):
             cadena += '1'
             gtabla(raiz.der, cadena)
 
+
 gtabla(bosque[0])
+
 
 def decodificar(cadena, arbol_huffman):
     decodificada, pos = '', 0
@@ -59,11 +67,13 @@ def decodificar(cadena, arbol_huffman):
         decodificada
     return decodificada
 
+
 def codificar(cadena, dic):
     codificada = ''
     for caracter in cadena:
         codificada += dic[caracter]
     return codificada
+
 
 if __name__ == "__main__":
     cadena = "TF103A0M"
