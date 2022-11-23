17349d8f3e6b3dc04f43fdddd4b508d4069fe132
jmedina28 <juanperezmedina67@gmail.com>
2022-11-23 21:57:40 +0100

    añado el diccionario

--- ejer1.py
+++ ejer1.py
@@ -1,3 +1,6 @@
 from arbolbinario import insertar, barridopostorden, nodoHuffman
 
+dic = {'A': '00', '3': '01', '1': '100', 'T': '110', 
+    'F': '111','0': '1010', 'M': '1011'}
+
 tabla = [['A', 0.2], ['F', 0.17], ['1', 0.13], ['3', 0.21], ['0', 0.05], ['M', 0.09], ['T', 0.15]]
