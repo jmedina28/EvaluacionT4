6f5a38771c1e6a2f2fa99e6ae99472108f30820e
jmedina28 <juanperezmedina67@gmail.com>
2022-11-24 22:24:13 +0100

    mejorando

--- ejer2.py
+++ ejer2.py
@@ -24,7 +24,7 @@ nombre = ['Bulbasaur', 'Charmander', 'Pikachu', 'Ivysaur', 'Charmeleon', 'Raichu
 'Charizard', 'Mewtwo', 'Squirtle', 'Wartortle', 'Blastoise', 'Mew', 'Eevee', 'Jolteon', 'Tyrantum', 'Lycanroc']
 
 for i in range (0, len(nombre)):
-    poke = Pokemon(nombre[i], randint(1, 100), choice(tipo), choice(debilidades))
+    poke = Pokemon(nombre[i].lower(), randint(1, 100), choice(tipo), choice(debilidades))
     a_nombres = insertar(a_nombres, [poke, poke.nombre])
     a_tipo = insertar(a_tipo, [poke, poke.tipo])
     a_numero = insertar(a_numero, [poke, poke.numero])
@@ -36,3 +36,14 @@ def orden_numero(raiz):
         orden_numero(raiz.der)
 print('El listado de todos los pokemons ordenados por número es el siguiente:')
 orden_numero(a_numero)
+
+def busqueda_prox_poke(raiz, busqueda):
+    if(raiz is not None):
+        if(raiz.informacion[1][0:len(busqueda)] == busqueda):
+            print(raiz.informacion[1])
+        busqueda_prox_poke(raiz.izq, busqueda)
+        busqueda_prox_poke(raiz.der, busqueda)
+
+x = input('Introduzca el nombre parcial que desea buscar:')
+print('El listado de los pokemons con ese nombre parcial es el siguiente:')
+busqueda_prox_poke(a_nombres, x.lower())
