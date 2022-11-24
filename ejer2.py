9dd74087ed34ef84c916a1e3c1d6f53bb7c9e9c5
jmedina28 <juanperezmedina67@gmail.com>
2022-11-24 20:05:18 +0100

    añado la base sobre la que voy a trabajar, luego añadire mas tarde el archivo con los 890 pokemons

--- ejer2.py
+++ ejer2.py
@@ -1,3 +1,20 @@
 import random
 from arbolbinario import nodoArbol, insertar
 from cola import Cola, vacia, atencion, arribo
+
+arbol_numero = None
+arbol_nombres = None
+arbol_tipo = None
+
+class Pokemon():
+    
+    def __init__(self, numero, nombre, tipo, debilidad):
+        self.numero = numero
+        self.nombre = nombre
+        self.tipo = tipo
+        self.debilidad = debilidad
+
+tipo = ['agua', 'fuego', 'tierra', 'electrico']
+debil = ['agua', 'fuego', 'tierra', 'electrico', 'Jolteon','Tyrantum', 'Lycanroc']
+nombre = ['Bulbasaur', 'Charmander', 'Pikachu', 'Ivysaur', 'Charmeleon', 'Raichu', 
+'Venusaur', 'Charizard', 'Mewtwo', 'Squirtle', 'Wartortle', 'Blastoise', 'Mew', 'Eevee', 'Jolteon', 'Tyrantum', 'Lycanroc']
