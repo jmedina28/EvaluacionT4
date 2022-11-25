d514d6f26aaecfec31451e1e05cd95bf44ab82db
jmedina28 <juanperezmedina67@gmail.com>
2022-11-25 12:49:56 +0100

    creo la estructura básica de mi grafo

--- grafos.py
+++ grafos.py
@@ -1,2 +1,17 @@
 from cola import Cola, vacia, atencion, arribo, nodoCola
 from monticulos import MonticuloMinimo
+
+def criterio(dato, campo=None):
+    dic = {}
+    if(hasattr(dato, '__dict__')):
+        dic = dato.__dict__
+    if(campo is None or campo not in dic):
+        return dato
+    else:
+        return dic[campo]
+
+class nodoArista():
+    informacion, siguiente, peso = None, None, None
+
+class nodoVertice():
+    informacion, siguiente, visitado, adyacentes, datos = None, None, False, None, None
