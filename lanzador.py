0e650df07bd6d2233ead90ad0c3fb34c82e69de0
jmedina28 <juanperezmedina67@gmail.com>
2022-11-25 12:21:48 +0100

    desarrollo el lanzador

--- lanzador.py
+++ lanzador.py
@@ -1,3 +1,6 @@
 
 def lanzar():
-    print("Aqui se lanzan los ejercicios")
+    x = int(input("""Introduzca el número del ejercicio que desea ejecutar: 
+    1. Ejercicio 1
+    2. Ejercicio 2
+    3. Ejercicio 3"""))
