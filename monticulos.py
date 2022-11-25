21842de1dd7ab16a437fecf5009ad7b6aec498dc
jmedina28 <juanperezmedina67@gmail.com>
2022-11-25 12:18:04 +0100

    creo la estructura del monticulo minimo

--- monticulos.py
+++ monticulos.py
@@ -1,4 +1,4 @@
-class MonticuloMax():
+class MonticuloMinimo():
 
     def __init__(self):
         self.vector, self.nelementos = [], 0
@@ -12,14 +12,14 @@ class MonticuloMax():
     def monticulo_lleno(self):
         return self.nelementos == len(self.vector)
 
-    def aniadir(self, dato, prioridad=1):
+    def aniadir(self, dato, prioridad=3):
         self.vector.append([dato, prioridad])
         self.flotar(self.nelementos)
         self.nelementos += 1
 
     def flotar(self, indice):
         padre = (indice -1) // 2
-        while(indice > 0 and self.vector[indice][1] > self.vector[padre][1]):
+        while(indice > 0 and self.vector[indice][1] < self.vector[padre][1]):
             self.vector[indice], self.vector[padre] = self.vector[padre], self.vector[indice]
             indice = padre
             padre = (indice -1) // 2
@@ -31,10 +31,10 @@ class MonticuloMax():
             mayor = h_izq
             h_der = h_izq + 1
             if h_der < self.nelementos:
-                if self.vector[h_der][1] > self.vector[h_izq][1]:
+                if self.vector[h_der][1] < self.vector[h_izq][1]:
                     mayor = h_der
             
-            if self.vector[indice][1] < self.vector[mayor][1]:
+            if self.vector[indice][1] > self.vector[mayor][1]:
                 self.vector[indice], self.vector[mayor] = self.vector[mayor], self.vector[indice]
                 indice = mayor
                 h_izq = (indice * 2) + 1
