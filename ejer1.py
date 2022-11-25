3aa0ba49f32c24b8315d0ee8837f7a8cc61447f7
jmedina28 <juanperezmedina67@gmail.com>
2022-11-25 12:28:38 +0100

    dejo acabado el lanzador del ejercicio 1

--- ejer1.py
+++ ejer1.py
@@ -1,37 +1,13 @@
 from arbolbinario import nodoHuffman
 from sys import getsizeof
 
-dic = {'A': '00', '3': '01', '1': '100', 'T': '110',
-       'F': '111', '0': '1010', 'M': '1011'}
-
-tabla = [['A', 0.2], ['F', 0.17], ['1', 0.13], [
-    '3', 0.21], ['0', 0.05], ['M', 0.09], ['T', 0.15]]
-
 
 def comparo(elemento):
     return elemento[1]
 
 
-tabla.sort(key=comparo)
-
-
 def comparacionnodo(elemento):
     return elemento.valor
-
-
-bosque = []
-
-for i in tabla:
-    nodo = nodoHuffman(i[0], i[1])
-    bosque.append(nodo)
-
-while(len(bosque) > 1):
-    elemento1 = bosque.pop(0)
-    elemento2 = bosque.pop(0)
-    nodo = nodoHuffman('', elemento1.valor+elemento2.valor)
-    nodo.izq, nodo.der = elemento1, elemento2
-    bosque.append(nodo)
-    bosque.sort(key=comparacionnodo)
 
 
 def gtabla(raiz, cadena=''):
@@ -44,9 +20,6 @@ def gtabla(raiz, cadena=''):
             cadena = cadena[0:-1]
             cadena += '1'
             gtabla(raiz.der, cadena)
-
-
-gtabla(bosque[0])
 
 
 def decodificar(cadena, arbol_huffman):
@@ -70,12 +43,3 @@ def codificar(cadena, dic):
     for caracter in cadena:
         codificada += dic[caracter]
     return codificada
-
-
-if __name__ == "__main__":
-    cadena = "TF103A0M"
-    codificada = codificar(cadena, dic)
-    print('La cadena codificada es la siguiente:' + str(codificada))
-    getsizeof(codificada), getsizeof(b"1101111001010010010101011")
-    decodificada = decodificar(codificada, bosque[0])
-    print('La cadena decodificada es la siguiente:' + str(decodificada))
--- lanzador.py
+++ lanzador.py
@@ -5,3 +5,33 @@ def lanzar():
     2. Ejercicio 2
     3. Ejercicio 3
     """))
+    if(x == 1):
+        from arbolbinario import nodoHuffman
+        from sys import getsizeof
+        from ejer1 import codificar, decodificar, gtabla, comparo, comparacionnodo
+        dic = {'A': '00', '3': '01', '1': '100', 'T': '110',
+       'F': '111', '0': '1010', 'M': '1011'}
+
+        tabla = [['A', 0.2], ['F', 0.17], ['1', 0.13], [
+            '3', 0.21], ['0', 0.05], ['M', 0.09], ['T', 0.15]]
+        tabla.sort(key=comparo)
+        bosque = []
+        for i in tabla:
+            nodo = nodoHuffman(i[0], i[1])
+            bosque.append(nodo)
+        while(len(bosque) > 1):
+            elemento1 = bosque.pop(0)
+            elemento2 = bosque.pop(0)
+            nodo = nodoHuffman('', elemento1.valor+elemento2.valor)
+            nodo.izq, nodo.der = elemento1, elemento2
+            bosque.append(nodo)
+            bosque.sort(key=comparacionnodo)
+        gtabla(bosque[0])
+        cadena = "TF103A0M"
+        codificada = codificar(cadena, dic)
+        print('La cadena codificada es la siguiente:' + str(codificada))
+        getsizeof(codificada), getsizeof(b"1101111001010010010101011")
+        decodificada = decodificar(codificada, bosque[0])
+        print('La cadena decodificada es la siguiente:' + str(decodificada))
+    elif(x == 2):
+        import ejer2
