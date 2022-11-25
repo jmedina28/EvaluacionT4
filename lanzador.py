fe7deccd3a19abfad662d1f222277e0127441faa
jmedina28 <juanperezmedina67@gmail.com>
2022-11-25 12:22:16 +0100

    añado un espacio

--- lanzador.py
+++ lanzador.py
@@ -3,4 +3,5 @@ def lanzar():
     x = int(input("""Introduzca el número del ejercicio que desea ejecutar: 
     1. Ejercicio 1
     2. Ejercicio 2
-    3. Ejercicio 3"""))
+    3. Ejercicio 3
+    """))
