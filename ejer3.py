36a6fc7a1b5b550061a38a79b1d0fd293b9d256c
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-11-25 18:50:28 +0100

    correcciones

--- ejer3.py
+++ ejer3.py
@@ -2,13 +2,13 @@ from grafos import Grafo
 
 grafo = Grafo(dirigido = False)
 
-grafo.insertar_vertice("E", datos ={"pais": "Grecia", "tipo":"Natural"})
-grafo.insertar_vertice("T", datos ={"pais":"Turquia", "tipo":"Natural"})
-grafo.insertar_vertice("M", datos ={"pais":"Turquia", "tipo":"Natural"})
-grafo.insertar_vertice("J", datos ={"pais":"Irak", "tipo":"Natural"})
-grafo.insertar_vertice("C", datos ={"pais":"Grecia", "tipo":"Natural"})
-grafo.insertar_vertice("F", datos ={"pais":"Egipto", "tipo":"Natural"})
-grafo.insertar_vertice("P", datos ={"pais":"Egipto", "tipo":"Natural"})
+grafo.insertar_vertice("E", datos ={"pais": "Grecia", "tipo":"natural"})
+grafo.insertar_vertice("T", datos ={"pais":"Turquia", "tipo":"natural"})
+grafo.insertar_vertice("M", datos ={"pais":"Turquia", "tipo":"natural"})
+grafo.insertar_vertice("J", datos ={"pais":"Irak", "tipo":"natural"})
+grafo.insertar_vertice("C", datos ={"pais":"Grecia", "tipo":"natural"})
+grafo.insertar_vertice("F", datos ={"pais":"Egipto", "tipo":"natural"})
+grafo.insertar_vertice("P", datos ={"pais":"Egipto", "tipo":"natural"})
 
 grafo.insertar_arista('E', 'T', 6)
 grafo.insertar_arista('E', 'M', 3)
